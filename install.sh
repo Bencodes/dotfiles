7c485e29f1a62394ff2e4250dd635db63fdfe1c1
Ben <ben@ben.cm>
2018-12-07 10:49:43 +0300

    Add the aliases to the install conf

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -9,3 +9,19 @@ git submodule update --init --recursive --quiet
 
 # Install everything
 "${BASEDIR}/dotbot/bin/dotbot" -d "${BASEDIR}" -c install.conf.yaml
+
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing 'sudo' time stamp until '.osx' has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Setup OSX
+./osx
+
+# Homebrew
+if ! which brew &> /dev/null; then
+  echo "Please install homebrew"
+  exit 1
+fi
