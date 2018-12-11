115ad9318d9a967e12ad7422e396594cc162a52c
Ben <ben@ben.cm>
2018-12-11 13:13:25 +0300

    Clean up the homebrew config some

--- install.sh
+++ install.sh
@@ -4,24 +4,23 @@ set -e
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Make sure we set up our submodules
-git submodule update --init --recursive --quiet
-
-# Install everything
-"${BASEDIR}/dotbot/bin/dotbot" -d "${BASEDIR}" -c install.conf.yaml
-
-
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing 'sudo' time stamp until '.osx' has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# Setup OSX
-./osx
-
-# Homebrew
+# Make sure we have homebrew installed
 if ! which brew &> /dev/null; then
-  echo "Please install homebrew"
+  echo "Please install homebrew https://brew.sh/"
   exit 1
 fi
+
+# Fetch our submodules
+git submodule update --init --recursive --quiet
+
+# Install the dotfiles
+"${BASEDIR}/dotbot/bin/dotbot" -d "${BASEDIR}" -c install.conf.yaml
+
+# Install the homebrew dependencies
+brew tap homebrew/bundle
+brew bundle --file=$BASEDIR/Brewfile.taps
+brew bundle --file=$BASEDIR/Brewfile.brews
+brew bundle --file=$BASEDIR/Brewfile.casks
+
+# Install the OSX configuration
+./osx
