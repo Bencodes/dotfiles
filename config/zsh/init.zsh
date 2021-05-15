d089bb5b65c8259052a56eca9a12333b1c3a4934
Benjamin Lee <ben@ben.cm>
2021-05-14 23:23:02 -0600

    Improve zsh init configuration some

--- config/zsh/init.zsh
+++ config/zsh/init.zsh
@@ -3,6 +3,33 @@
 source $(dirname ${(%):-%x})/antigen/antigen.zsh
 source ~/.config/zsh/theme.zsh
 
+# History settings
+setopt APPEND_HISTORY
+setopt INC_APPEND_HISTORY
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_REDUCE_BLANKS
+setopt HIST_VERIFY
+# Save x items to the given history file
+HISTSIZE=1000000
+SAVEHIST=1000000
+HISTFILE=$HOME/.zsh_history
+
+# Dir settings
+setopt AUTO_PUSHD
+setopt PUSHD_IGNORE_DUPS
+setopt PUSHD_TO_HOME
+setopt PUSHD_MINUS
+setopt PUSHD_SILENT
+setopt CDABLE_VARS
+DIRSTACKSIZE=20
+
+# Warn before quitting
+setopt CHECK_JOBS
+
+# Use vim shortcuts within the terminal (defaults to insert mode)
+bindkey -v
+
 antigen use oh-my-zsh
 antigen bundle adb
 antigen bundle brew
