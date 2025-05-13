11de71c012405b9eb797a2261d9db2ee217c377b
Ben Lee <ben@ben.cm>
2025-05-13 08:56:55 -0700

    Fix config/nvim/lua/theme.lua deprecations

--- config/nvim/lua/theme.lua
+++ config/nvim/lua/theme.lua
@@ -1,18 +1,20 @@
 require("github-theme").setup({
-  function_style = "italic",
-  sidebars = {"qf", "vista_kind", "terminal", "packer"},
+	options = {
+		function_style = "italic",
+		sidebars = {"qf", "vista_kind", "terminal", "packer"},
 
-  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-  colors = {hint = "orange", error = "#ff0000"},
+		-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+		colors = {hint = "orange", error = "#ff0000"},
 
-	vim.cmd('colorscheme github_dark'),
-  -- Overwrite the highlight groups
-  overrides = function(c)
-    return {
-      htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
-      DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
-      -- this will remove the highlight groups
-      TSField = {},
-    }
-  end
+		vim.cmd('colorscheme github_dark'),
+		-- Overwrite the highlight groups
+		overrides = function(c)
+			return {
+				htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
+				DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
+				-- this will remove the highlight groups
+				TSField = {},
+			}
+		end
+	}
 })
